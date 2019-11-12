29aba7d9bbaddb39670e15c725848f6950c4e295
crkubiak <charles@8thlight.com>
2019-11-12 13:41:43 -0600

    created beginning build of main activity, created capture/view activities

---
+++ app/src/main/java/com/crkubiak/android/catstonefrontend/CaptureActivity.kt
@@ -0,0 +1,12 @@
+package com.crkubiak.android.catstonefrontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CaptureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capture)
+    }
+}
---
+++ app/src/main/java/com/crkubiak/android/catstonefrontend/RecordsActivity.kt
@@ -0,0 +1,12 @@
+package com.crkubiak.android.catstonefrontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_records)
+    }
+}
