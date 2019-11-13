31aa7bbb8c20bbdc84e9d115a345371cfe4d567f
crkubiak <charles@8thlight.com>
2019-11-12 21:58:27 -0600

    added listeners and basic components to capture, still need to style

--- app/src/main/java/com/crkubiak/android/catstonefrontend/MainActivity.kt
+++ app/src/main/java/com/crkubiak/android/catstonefrontend/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.crkubiak.android.catstonefrontend
 
+import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var purr = true
+        capture_cat_button.setOnClickListener {
+            val intent = Intent(this, CaptureActivity::class.java)
+            startActivity(intent)
+        }
 
+        view_captures_button.setOnClickListener {
+            val intent = Intent(this, RecordsActivity::class.java)
+            startActivity(intent)
+        }
 
+        main_image.setOnClickListener {
+            if (purr) {
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.purr)
+                mediaPlayer?.start()
+            }
+            purr = false
+        }
     }
 }
